013f4b1e45316f3d1f80d2cee21872253121c623
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-11-12 21:59:46 +0400

    create components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import mario from '../images/mario.png'
+
+import {Posts} from './Posts/Posts.jsx'
+import {Form} from './Form/Form.jsx'
 
 export const App = () => {
   return (
-    <h1>
-      MERN
-    </h1>
+    <Container maxWidth='lg'>
+      <AppBar position="static" color='inherit'>
+        <Typography variant='h2' align='center'> Mario  </Typography>
+        <img src={mario} alt='mario' width='350'  height='350'/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch'>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid >
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid >
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
---
+++ client/src/components/Posts/Posts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Post} from './Post/Post.jsx'
+
+export const Posts = () => {
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post />
+        </>
+    )
+}
