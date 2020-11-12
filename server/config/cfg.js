79925dcadb06ed75d783fd05954aa114f1d0f909
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-11-12 21:30:53 +0400

    initial commit

---
+++ server/config/cfg.js
@@ -0,0 +1,7 @@
+export const PORT = process.env.PORT || 5011,
+    M_DB_URL = process.env.PORT || "mongodb://localhost:27017/mern",
+    M_OPTION = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    }
