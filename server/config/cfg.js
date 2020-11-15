63152949427fefa9b1e51d6ea3458e4a39c985c2
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-11-15 20:08:51 +0400

    mod: server varibale, client responsible

--- server/config/cfg.js
+++ server/config/cfg.js
@@ -1,5 +1,7 @@
+import dotenv from 'dotenv'
+
 export const PORT = process.env.PORT || 5011,
-    M_DB_URL = process.env.PORT || "mongodb://localhost:27017/mern",
+    M_DB_URL = process.env.M_DB_URL || "mongodb://localhost:27017/mern",
     M_OPTION = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
